3791e0b881e94f2766834eeea83ea9bc7c726e84
Pallavi Mandloi <ankit.doc@hotmail.com>
2024-12-22 22:48:35 +0530

    ep-4 p-4 added dynamic cards in props from swiggiy data

--- app.js
+++ app.js
@@ -13,6 +13,217 @@ import ReactDOM from "react-dom/client";
   --links
 */
 
+const resList = {
+                "info": {
+                      "id": "986926",
+                      "name": "Chinese Wok",
+                      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/7/cd209413-fca7-4448-9214-6887c4773d06_986926.JPG",
+                      "locality": "Secunderabad",
+                      "areaName": "Shivaji Nagar",
+                      "costForTwo": "₹250 for two",
+                      "cuisines": [
+                        "Chinese",
+                        "Asian",
+                        "Tibetan",
+                        "Desserts"
+                      ],
+                      "avgRating": 4.4,
+                      "parentId": "61955",
+                      "avgRatingString": "4.4",
+                      "totalRatingsString": "35",
+                      "sla": {
+                        "deliveryTime": 38,
+                        "lastMileTravel": 4.7,
+                        "serviceability": "SERVICEABLE",
+                        "slaString": "35-40 mins",
+                        "lastMileTravelString": "4.7 km",
+                        "iconType": "ICON_TYPE_EMPTY"
+                      },
+                      "availability": {
+                        "nextCloseTime": "2024-12-22 23:00:00",
+                        "opened": true
+                      },
+                      "badges": {
+                        
+                      },
+                      "isOpen": true,
+                      "type": "F",
+                      "badgesV2": {
+                        "entityBadges": {
+                          "imageBased": {
+                            
+                          },
+                          "textBased": {
+                            
+                          },
+                          "textExtendedBadges": {
+                            
+                          }
+                        }
+                      },
+                      "aggregatedDiscountInfoV3": {
+                        "header": "ITEMS",
+                        "subHeader": "AT ₹149"
+                      },
+                      "orderabilityCommunication": {
+                        "title": {
+                          
+                        },
+                        "subTitle": {
+                          
+                        },
+                        "message": {
+                          
+                        },
+                        "customIcon": {
+                          
+                        }
+                      },
+                      "differentiatedUi": {
+                        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                        "differentiatedUiMediaDetails": {
+                          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                          "lottie": {
+                            
+                          },
+                          "video": {
+                            
+                          }
+                        }
+                      },
+                      "reviewsSummary": {
+                        
+                      },
+                      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+                      "isNewlyOnboarded": true,
+                      "restaurantOfferPresentationInfo": {
+                        
+                      },
+                      "externalRatings": {
+                        "aggregatedRating": {
+                          "rating": "--"
+                        }
+                      },
+                      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+                    },
+                    "info": {
+                      "id": "24613",
+                      "name": "Domino's Pizza",
+                      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/29/024e7f68-ac87-413c-a453-fbb6328820d2_24613.jpg",
+                      "locality": "Stock Exchange Road",
+                      "areaName": "Himayath Nagar",
+                      "costForTwo": "₹400 for two",
+                      "cuisines": [
+                        "Pizzas",
+                        "Italian",
+                        "Pastas",
+                        "Desserts"
+                      ],
+                      "avgRating": 4.4,
+                      "parentId": "2456",
+                      "avgRatingString": "4.4",
+                      "totalRatingsString": "19K+",
+                      "sla": {
+                        "deliveryTime": 25,
+                        "lastMileTravel": 1.3,
+                        "serviceability": "SERVICEABLE",
+                        "slaString": "20-25 mins",
+                        "lastMileTravelString": "1.3 km",
+                        "iconType": "ICON_TYPE_EMPTY"
+                      },
+                      "availability": {
+                        "nextCloseTime": "2024-12-23 02:59:00",
+                        "opened": true
+                      },
+                      "badges": {
+                        "imageBadges": [
+                          {
+                            "imageId": "bolt/Bolt%20Listing%20badge@3x.png",
+                            "description": "bolt!"
+                          },
+                          {
+                            "imageId": "Rxawards/_CATEGORY-Pizza.png",
+                            "description": "Delivery!"
+                          }
+                        ]
+                      },
+                      "isOpen": true,
+                      "type": "F",
+                      "badgesV2": {
+                        "entityBadges": {
+                          "imageBased": {
+                            "badgeObject": [
+                              {
+                                "attributes": {
+                                  "description": "bolt!",
+                                  "imageId": "bolt/Bolt%20Listing%20badge@3x.png"
+                                }
+                              },
+                              {
+                                "attributes": {
+                                  "description": "Delivery!",
+                                  "imageId": "Rxawards/_CATEGORY-Pizza.png"
+                                }
+                              }
+                            ]
+                          },
+                          "textBased": {
+                            
+                          },
+                          "textExtendedBadges": {
+                            
+                          }
+                        }
+                      },
+                      "aggregatedDiscountInfoV3": {
+                        "header": "ITEMS",
+                        "subHeader": "AT ₹89"
+                      },
+                      "orderabilityCommunication": {
+                        "title": {
+                          
+                        },
+                        "subTitle": {
+                          
+                        },
+                        "message": {
+                          
+                        },
+                        "customIcon": {
+                          
+                        }
+                      },
+                      "differentiatedUi": {
+                        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                        "differentiatedUiMediaDetails": {
+                          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                          "lottie": {
+                            
+                          },
+                          "video": {
+                            
+                          }
+                        }
+                      },
+                      "reviewsSummary": {
+                        
+                      },
+                      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+                      "restaurantOfferPresentationInfo": {
+                        
+                      },
+                      "externalRatings": {
+                        "aggregatedRating": {
+                          "rating": "3.9",
+                          "ratingCount": "2.5K+"
+                        },
+                        "source": "GOOGLE",
+                        "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+                      },
+                      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+                    }
+                  
+                  };
 const Header = () => {
   return (
     <div className="header">
@@ -31,20 +242,22 @@ const Header = () => {
   )
 };
 
+
 const RestaurantCard = (props) => {
   {console.log(props)}
-  let {name,cuisines,avgRating,timer} = props;
+  let {resdata} = props;
   return (
     <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
       <img 
       className="res-logo"
       alt="re-logo"
-      src="https://b.zmtcdn.com/data/pictures/3/21048793/1fcfb196ba04f2ec5d953c125bcaad44.jpeg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"
+      src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+ resdata.info.cloudinaryImageId}
       />
-      <h3> {name}</h3>
-      <h4>{cuisines}</h4>
-      <h4>{avgRating} </h4>
-      <h4> {timer}</h4>
+      <h3> {resdata.info.name}</h3>
+      <h4>{resdata.info.cuisines.join(", ")}</h4>
+      <h4>{resdata.info.costForTwo}</h4>
+      <h4>{resdata.info.avgRating} </h4>
+      <h4> {resdata.info.sla.deliveryTime}</h4>
     </div>
   )
 }
@@ -54,7 +267,8 @@ return (
   <div className="body">
     <div className="search" > Search</div>
     <div className="res-container">
-      <RestaurantCard name="Kanika Kitchen" cuisines="punjabi" avgRating="4.5" timer="35 min"  />
+      <RestaurantCard resdata = {resList} />
+      <RestaurantCard resdata = {resList} />
     </div>
   </div>
 )
