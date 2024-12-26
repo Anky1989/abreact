69898baf845d3d1574a88943ed46d6719e6b4524
Pallavi Mandloi <ankit.doc@hotmail.com>
2024-12-26 11:13:52 +0530

    ep-5 p-2 add filter and clear button using useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
 const Body = () => {
+  [listOfRestaurants, setResList] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search"> Search</div>
+      <div className="search">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.2
+            );
+            setResList(filteredList);
+          }}
+        >
+          Fitler Top Restaurant
+        </button>
+        <button className="clear-btn" onClick={() => setResList(resList)}>
+          Clear Filter
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resdata={restaurant} />
         ))}
       </div>
