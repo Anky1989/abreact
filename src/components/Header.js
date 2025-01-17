23f573589e04dbbe04821271519f90a7d33506d0
Pallavi Mandloi <ankit.doc@hotmail.com>
2025-01-17 14:48:01 +0530

    ep-6 p-6 login logout done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,9 @@ import { LOGO_URL } from "../utils/contants";
 import { useState, useEffect } from "react";
 
 export const Header = () => {
-  let btnName = "Login";
-
-  console.log("btnName", btnName);
-
   [btnName, setBtnName] = useState("Login");
+
+  console.log("Header render"); // complete component render when state changes
 
   return (
     <div className="header">
@@ -22,7 +20,8 @@ export const Header = () => {
           <button
             className="login"
             onClick={() => {
-              btnName = "Logout";
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+              console.log("btnName", btnName);
             }}
           >
             {btnName}
